03f0a28cfc86f0fe8ef6fa21a63890a7787b835c
ValiullinAd <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>
2022-04-10 08:33:08 +0300

    initial commit

---
+++ task.js
@@ -0,0 +1,28 @@
+"use strict";
+function solveEquation(a, b, c) {
+  let arr;
+  let D = (b * b)- 4 * a * c;
+  if (D < 0) {
+    arr = [];
+  }
+  if (D === 0) {
+    let x = -b / (2 * a);
+    arr = [x];
+  }
+  if (D > 0) {
+    let x1 = -b + Math.sqrt((b * b)- 4 * a * c);
+    let x2 = -b - Math.sqrt((b * b)- 4 * a * c);
+    arr = [x1,x2];
+  }
+
+  // код для задачи №1 писать здесь
+  return arr; // array
+}
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
